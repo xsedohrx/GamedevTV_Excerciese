586615f436e5957acac0d809d69ea90384564ec6
Luke Torre <luke.torre@hotmail.com>
2026-05-16 21:44:40 +0200

    Working Launchpad
    add force function to bouncybouncy to launch player.

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // GameDev.tv Challenge Club. Got questions or want to share your nifty solution?
-// Head over to - http://community.gamedev.tv
+// Head over to - http://community.gamedev.tv/
 
 public class Bounce : MonoBehaviour
 {
     [SerializeField] float jumpForce = 1000f;
-    
-    void OnTriggerEnter(Collider other)
+
+    void OnTriggerEnter(Collider pannenkoek)
     {
-        // Challenge 1: JumpyJumpy(other);
+
+        JumpyJumpy(pannenkoek);
     }
 
-    void JumpyJumpy(Collider other)
+    void JumpyJumpy(Collider collisionObject)
     {
+        if (collisionObject.tag == "Player")
+        {
+            //IF Player collides with pad
+            //Player gets lauched
 
+            collisionObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+            Debug.Log("PLayer collides with pad");
+        }
+        else if (collisionObject.tag == "Enemy")
+        {
+            Debug.Log("Detected an Enemy");
+        }
+        else
+        {
+            Debug.Log("Anything else");
+        }
     }
 }
