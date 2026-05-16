6fcf194db053cb6f7705bb426ea0e9a32f9d6457
Luke Torre <luke.torre@hotmail.com>
2026-05-16 23:06:56 +0200

    Non-working coroutine
    sucks

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -7,10 +7,17 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
+
+
+    // Tracks if this specific teleporter is currently on cooldown
+    private bool isCoolingDown = false;
     [SerializeField] Transform teleportTarget;
     [SerializeField] GameObject player;
     [SerializeField] Light areaLight;
     [SerializeField] Light mainWorldLight;
+    
+    [Tooltip("How long to wait before allowing another teleportation.")]
+    [SerializeField] float cooldownTime = 3f;
 
     void Start() 
     {
@@ -20,21 +27,50 @@ public class Teleport : MonoBehaviour
 
     void OnTriggerEnter(Collider other) 
     {
-        // Challenge 2: TeleportPlayer();
+        if (isCoolingDown == true)
+        {
+            return;
+        }
+        TeleportPlayer(other,player,teleportTarget);
         // Challenge 3: DeactivateObject();
         // Challenge 4: IlluminateArea();
         // Challenge 5: StartCoroutine ("BlinkWorldLight");
         // Challenge 6: TeleportPlayerRandom();
     }
+    
 
-    void TeleportPlayer()
+
+    // Public method so other teleporters can trigger this one's cooldown
+    public void StartCooldown()
     {
-        // code goes here
+        StartCoroutine(CooldownRoutine());
     }
 
+    // The Coroutine that handles the 3-second wait
+    private IEnumerator CooldownRoutine()
+    {
+        isCoolingDown = true;
+
+        // Pause execution of this coroutine for 'cooldownTime' seconds
+        yield return new WaitForSeconds(cooldownTime);
+
+        isCoolingDown = false;
+    }
+    void TeleportPlayer(Collider other, GameObject player, Transform teleportTarget)
+    {
+        
+
+        // when player collides with teleportA (after standing on it for 3 seconds),
+        // spawn at teleportB
+        if (other.tag == "Player")
+        {
+            player.transform.position = teleportTarget.position;
+
+        }
+    }
     void DeactivateObject()
     {
-       // code goes here 
+       // If player spawns on teleporter, deactivate until collision = false
     }
 
     void IlluminateArea()
